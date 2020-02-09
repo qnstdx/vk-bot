573eaefbd1bb8571a6964ccfd6e6b9e7f33d5226
Nikita <superduperproger@gmail.com>
2020-02-09 14:49:55 +0300

    "Update"

--- app/controllers/BotController.php
+++ app/controllers/BotController.php
@@ -4,6 +4,9 @@ class BotController
 {
     public function actionBotWork ()
     {
+        $data = json_decode ( file_get_contents ( 'php://input' ) );
+
+        print_r($data);
         return '52503fa1';
     }
 }
