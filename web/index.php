9a8292a3fdb10c61988e31bc30cea3623d3e6bf4
Nikita <superduperproger@gmail.com>
2020-02-09 15:59:14 +0300

    Fix errors

--- web/index.php
+++ web/index.php
@@ -5,12 +5,6 @@ require('../vendor/autoload.php');
 $app = new Silex\Application();
 $app['debug'] = true;
 
-$conf = [
-	'VK_TOKEN' => 'b08a880df0df92902b33fe22cf60e3efa8ac6e8fd99e189feb3ef0f0e2bc5529578a691a95045506b532a',
-	'VK_SECRET_TOKEN' => 'qwezHGDSJFhgjhs',
-	'VK_CONF_CODE' => '52503fa1'
-];
-
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
@@ -20,6 +14,12 @@ $app->get('/', function() use($app) {
 });
 
 $app->post('/bot', function() use($app) {
+	$conf = [
+		'VK_TOKEN' => 'b08a880df0df92902b33fe22cf60e3efa8ac6e8fd99e189feb3ef0f0e2bc5529578a691a95045506b532a',
+		'VK_SECRET_TOKEN' => 'qwezHGDSJFhgjhs',
+		'VK_CONF_CODE' => '52503fa1'
+	];
+
 	$data = json_decode(file_get_contents('php://input'));
 
 	if (!$data) 
