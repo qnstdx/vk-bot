3509c50bc6107d1e309aeb1c30f47ff524290e19
Nikita <superduperproger@gmail.com>
2020-02-09 13:23:02 +0300

    First deploy!

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+/**
+* |-------------------------------------|
+* |                                     |
+* |            Vue JS code              |
+* |   powered by https://vuejs.org      |
+* |                                     |
+* |-------------------------------------|
+* */
+let app = new Vue({
+    el: '#app',
+    // Чтобы не было конфликта с Twig
+    delimiters: ['${', '}'],
+    data: {
+        vue_answer: 'VueJS Worked!',
+        data_last_update: 'Last update: 18.01.2020',
+        seen: true
+    },
+    methods: {
+        opacityGrappy: function () {
+            this.seen = false
+        }
+    }
+});
