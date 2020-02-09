0affc8113bc014f318cae950044f905e7c967e87
Nikita <superduperproger@gmail.com>
2020-02-09 15:20:52 +0300

    Test silex

--- web/index.php
+++ web/index.php
@@ -18,8 +18,7 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 // Our web handlers
 
 $app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+  return 'Hello, world!';
 });
 
 $app->run();
