19d1ab77349bf928ed4d388718a1b3a990962b54
Nikita <superduperproger@gmail.com>
2020-02-09 14:42:24 +0300

    Upadte controller

--- app/controllers/BotController.php
+++ app/controllers/BotController.php
@@ -4,6 +4,13 @@ class BotController
 {
     public function actionBotWork ()
     {
+        if ( ! empty ( $_POST ) )
+        {
+            return '52503fa1';
+        } else {
+            return 'noih';
+        }
+
         return true;
     }
 }
