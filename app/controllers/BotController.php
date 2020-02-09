05d847702ab004ce47cf3584ead90aa403c52a56
Nikita <superduperproger@gmail.com>
2020-02-09 14:45:31 +0300

    Changes

--- app/controllers/BotController.php
+++ app/controllers/BotController.php
@@ -4,13 +4,6 @@ class BotController
 {
     public function actionBotWork ()
     {
-        if ( ! empty ( $_POST ) )
-        {
-            echo '52503fa1';
-        } else {
-            echo 'noih';
-        }
-
-        return true;
+        return '52503fa1';
     }
 }
